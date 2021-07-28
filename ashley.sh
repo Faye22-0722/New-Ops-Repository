2cfa8abe6a548611348b91264de9bf79f413e550
Faye2-0722 <ashley.lucas22@gmail.com>
2021-07-28 13:33:50 -1000

    ashley.sh

---
+++ ashley.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+myipsettings=$(ifconfig)
+ 
+echo "myipsettings" > output.txt
+echo $myipsettings > output.txt
+ 
+#script: ashley.sh
+#author: Ashley
+#last revision: 2021/07/28
+#purpose: my network configurations
+#no variables
+#no functions
+#main
+#end
+
